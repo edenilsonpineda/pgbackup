815c5606a5edd1a71ac2fe7eff3b0366b8ca2ca1
Edenilson Pineda <calberto@hightech-corp.com>
2022-02-22 20:28:31 -0600

    add file name generation with timestamp

--- src/pgbackup/cli.py
+++ src/pgbackup/cli.py
@@ -1,3 +1,4 @@
+import time
 from argparse import Action, ArgumentParser
 
 
@@ -17,6 +18,7 @@ def create_parser():
     parser.add_argument("--driver",
                         help="How & Where to store the backup",
                         nargs=2,
+                        metavar=("DRIVER", "DESTINATION"),
                         action=DriverAction,
                         required=True)
     return parser
@@ -28,9 +30,16 @@ def main():
 
     args = create_parser().parse_args()
     dump = pgdump.dump(args.url)
+
     if args.driver == 's3':
         client = boto3.client('s3')
-        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+        timestamp = time.strftime("%Y-%m-%dT%H-%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+
+        print(f"Backing database up to {args.destination} in S3 as {file_name}")
+
+        storage.s3(client, dump.stdout, args.destination, file_name)
     else:
         outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {args.destination}")
         storage.local(dump.stdout, outfile)
--- src/pgbackup/pgdump.py
+++ src/pgbackup/pgdump.py
@@ -8,3 +8,12 @@ def dump(url):
     except OSError as err:
         print(f"Error: {err}")
         sys.exit(1)
+
+
+def dump_file(url: str, timestamp=None):
+    db_name = url.split('/')[-1]  # postgres://demo:pass@ip:port/dbname (Removes any slashes and return the last item
+    db_name = db_name.split('?')[0]
+    if timestamp:
+        return f"{db_name}-{timestamp}.sql"
+    else:
+        return f"{db_name}.sql"
